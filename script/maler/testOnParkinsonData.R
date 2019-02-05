66c5b22e26765c503d23787cba612dedaefbc037
Nima Ramezani Taghiabadi <nima@Nimas-MacBook-Pro.local>
2019-02-05 17:23:01 +1100

    keras tutorials started

--- script/maler/testOnParkinsonData.R
+++ script/maler/testOnParkinsonData.R
@@ -2,18 +2,34 @@ library(magrittr)
 library(gener)
 library(smbinning)
 
+source('~/Documents/software/R/packages/maler/R/abstract.R')
+source('~/Documents/software/R/packages/maler/R/transformers.R')
+source('~/Documents/software/R/packages/maler/R/classifiers.R')
+source('~/Documents/software/R/packages/maler/R/regressors.R')
+ 
 dataset = read.table('~/Documents/data/uci_repo/emg_data_7Jan19/EMG_data_for_gestures-master/01/1_raw_data_13-12_22.03.16.txt', header = T)
 dataset$class %<>% as.integer
 dataset$Y0 = (dataset$class == 0) %>% as.integer
 
-
-
-
-sm = SMBINNING()
-
 columns = names(dataset) %>% setdiff(c('time', 'class', 'Y0'))
 dataset[, columns] %<>% {.*100000}
 
-lr = SCIKIT.LR(transformer = DUMMIFIER(transformer = SMBINNING()))
+sm = SMBINNING()
+#sm$fit(X = dataset[columns], y = dataset[, 'Y0'])
+
+dm = DUMMIFIER(transformer = sm, settings = list(transformer = list(keep_original = T)))
+
+#dm$fit(X = dataset[columns], y = dataset[, 'Y0'])
+
+#cr = CATREMOVER(transformer = dm())
+#X2 = dm$predict(dataset[columns])
+
+lr = SCIKIT.LR(transformer = dm, settings = list(cross_validation = list(reset_transformer = F)))
 
 lr$fit(X = dataset[columns], y = dataset[, 'Y0'])
+lr$get.performance.cv(X = dataset[columns], y = dataset[, 'Y0'])
+
+dt = SCIKIT.DT(transformer = dm, settings = list(cross_validation = list(reset_transformer = F)))
+dt$fit(X = dataset[columns], y = dataset[, 'Y0'])
+
+dt$get.performance.cv(X = dataset[columns], y = dataset[, 'Y0'])
