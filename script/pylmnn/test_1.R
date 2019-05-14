92bbe3877289f326714dab573381a19e231e3db8
Nicholas Berta <45607424+genpack@users.noreply.github.com>
2019-05-14 10:37:59 +1000

    pymnn test added

--- script/pylmnn/test_1.R
+++ script/pylmnn/test_1.R
@@ -1,1 +1,15 @@
 library(magrittr)
+library(dplyr)
+library(reticulate)
+
+
+knn_module  <- import('sklearn.neighbors')
+lmnn_module <- import('pylmnn')
+
+iris %>% partition(0.7) -> part
+
+X_train = part$part1 %>% dplyr::select(-Species)
+y_train = part$part1 %>% pull(Species)
+X_test  = part$part2 %>% dplyr::select(-Species)
+y_test  = part$part2 %>% pull(Species)
+
