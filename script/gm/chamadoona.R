0e62f13d677ec2760e8579574769ab5462bc7500
Nima Ramezani <nima_ramezani@hotmail.com>
2023-02-22 13:17:30 +1100

    gm test scripts updates

--- script/gm/chamadoona.R
+++ script/gm/chamadoona.R
@@ -3,7 +3,8 @@ library(dplyr)
 library(gm)
 
 
-read.csv('data/chamadoona.csv') -> mel
+read.csv('script/gm/data/chamadoona.csv') -> mel
+# mel %>% write.csv('script/gm/data/chamadoona.csv', row.names = F)
 
 View(mel)
 
@@ -11,13 +12,16 @@ is_issue = mel %>% group_by(measure) %>% summarise(sumdur = sum(duration)) %>%
   pull(sumdur) %>% {.!=1} %>% sum %>% as.logical
 is_issue = is_issue & (is.na(mel$octave[mel$note != 'r']) %>% sum)
 
-
+mel %<>% filter(duration > 0)
 mel$pitch = mel$note %>% stringr::str_replace(pattern = '_', replacement = '-') %>% 
   toupper %>% paste0(mel$octave)
 mel$pitch[mel$note == 'r'] <- NA
 
-melody = Line(pitches = mel$pitch %>% as.list, durations = mel$durint %>% as.list, name = 'Melody')
+melody = Line(pitches = mel$pitch %>% as.list, durations = (mel$duration/2) %>% as.list, name = 'Melody')
 
 m = Music() + Meter(4,4) + Key(-2) + melody + Clef('G', to = "Melody")
 
 show(m)
+show(m, 'audio')
+
+m %>% export(dir_path = 'script/gm/data', file_name = 'chamadoona', formats = 'mid')
